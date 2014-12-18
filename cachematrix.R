c63339411a804d65352cd6af209d6890196a6384
Maytrics <maytrics@outlook.com>
2014-12-17 20:53:29 -0800

    Cached Inversion Implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+# -----------------------------------------------------------------------------
+## cacheMatrix.R - Making matrix inversion faster via caching.
+## Author: Maytrix
+## Project: Coursera; R-Programming; December 2014
+# -----------------------------------------------------------------------------
 
-## Write a short comment describing this function
+# -----------------------------------------------------------------------------
+## makeCacheMatrix - A cached matrix object.
+## 	set - store orginal matrix data
+##	get - original matrix data
+##	setcache - cache (store) inverted matrix
+##	getcache - retrieve inverted matrix
+# -----------------------------------------------------------------------------
 
 makeCacheMatrix <- function(x = matrix()) {
+	cachem <- NULL
+	set <- function (y) {
+		x <<- y
+		# The matrix has changed. Cache no longer valid.
+		cachem <<- NULL
+	}
+	get <- function() x
+	setcache <- function(c) cachem <<- c
+	getcache <- function() cachem
+	list(set = set, get = get, 
+	     setcache = setcache, getcache = getcache)
 
-}
+} # makeCacheMatrix
 
 
-## Write a short comment describing this function
+# -----------------------------------------------------------------------------
+## cacheSolve -- Cached Matrix Inversion. Makes solve() efficient with caching.
+## Returns cached copy of inverted matrix is available.
+##         Otherwise returns a freshly computed inverse.
+# -----------------------------------------------------------------------------
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+        ## Access cache. 
+	cachem <- x$getcache ()
+	
+	if (!is.null(cachem)){
+		# Found a cached copy.
+		message ("Cached inverse:")
+		return (cachem)
+	}
+
+	# Cache is empty. Compute the inverse.
+	data <- x$get()
+	cachem <- solve (data)
+	x$setcache(cachem)
+	cachem
+} # cacheSolve
+
+
